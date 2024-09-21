1c546801847c13539ee24eb0ebcf8d4d1ebd17ea
BrianLo <66210536+scs2007022@users.noreply.github.com>
2024-09-21 22:36:55 +0800

    test

--- src/dart-and-flutter/install.sh
+++ src/dart-and-flutter/install.sh
@@ -2,7 +2,6 @@ apt-get update && apt-get install -y apt-transport-https curl git unzip xz-utils
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --batch --yes --dearmor -o /usr/share/keyrings/dart.gpg
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | tee /etc/apt/sources.list.d/dart_stable.list
 apt-get update 
-export DART_VERSION='latest'
 echo "$DART_VERSION"
 if [ $DART_VERSION == 'latest' ];
 then
