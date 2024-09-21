a592f4582dfc0b4bf9bf0acc328ea3d0c9ae01aa
BrianLo <66210536+scs2007022@users.noreply.github.com>
2024-09-21 22:50:30 +0800

    test

--- src/dart-and-flutter/install.sh
+++ src/dart-and-flutter/install.sh
@@ -3,7 +3,7 @@ wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --batch --
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | tee /etc/apt/sources.list.d/dart_stable.list
 apt-get update 
 echo "$DART_VERSION"
-if [ $DART_VERSION == 'latest' ];
+if [ "$DART_VERSION" == 'latest' ];
 then
     echo "apt-get install -y dart"
     apt-get install -y dart
@@ -11,7 +11,7 @@ else
     echo "apt-get install -y dart=$DART_VERSION"
     apt-get install -y dart=$DART_VERSION
 fi
-if [ $FLUTTER == true ];
+if [ "$FLUTTER" == true ];
 then
     echo "Installing Flutter SDK"
     git clone https://github.com/flutter/flutter.git -b stable --depth 1 /flutter
