d20c501768879fbeaf901c0ca09dba3640806785
BrianLo <66210536+scs2007022@users.noreply.github.com>
2024-09-28 04:51:25 +0000

    fix apt install -y

--- src/dart-and-flutter/install.sh
+++ src/dart-and-flutter/install.sh
@@ -20,6 +20,6 @@ fi
 if [[ "$install-google-executable" = true ]];
 then
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    apt install ./google-chrome-stable_current_amd64.deb
+    apt install -y ./google-chrome-stable_current_amd64.deb
 fi
 apt-get clean
