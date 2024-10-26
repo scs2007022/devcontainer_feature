c483243d114c9381e98431d300093d28e7a27125
BrianLo <66210536+scs2007022@users.noreply.github.com>
2024-10-26 01:48:49 +0000

    dart --version and flutter --version at the end

--- src/dart-and-flutter/install.sh
+++ src/dart-and-flutter/install.sh
@@ -11,10 +11,12 @@ then
 else
     echo "apt-get install -y dart=$DART_VERSION"
     sudo apt-get install -y dart=$DART_VERSION
+    dart --version
 fi
 if [[ "$INSTALL_FLUTTER" = true ]];
 then
     echo "Installing Flutter SDK"
     git clone https://github.com/flutter/flutter.git -b stable --depth 1 /flutter
+    flutter --version
 fi
 sudo apt-get clean
