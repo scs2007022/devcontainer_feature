8480ff57efe5f14f188b66ae39c019ed04e32fd7
Brian Lo <66210536+scs2007022@users.noreply.github.com>
2024-12-16 01:17:09 +0800

    Update install.sh

--- src/dart-and-flutter/install.sh
+++ src/dart-and-flutter/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl git unzip xz-utils zip libglu1-mesa wget
+apt-get update && apt-get install -y apt-transport-https curl git unzip xz-utils zip libglu1-mesa wget
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/dart.gpg
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
-sudo apt-get update 
+apt-get update 
 echo "$DART_VERSION"
 if [[ "$DART_VERSION" = 'latest' ]];
 then
     echo "apt-get install -y dart"
-    sudo apt-get install -y dart
+    apt-get install -y dart
 else
     echo "apt-get install -y dart=$DART_VERSION"
-    sudo apt-get install -y dart=$DART_VERSION
+    apt-get install -y dart=$DART_VERSION
     dart --version
 fi
 if [[ "$INSTALL_FLUTTER" = true ]];
@@ -18,7 +18,7 @@ then
     echo "Installing Flutter SDK"
     git clone https://github.com/flutter/flutter.git -b stable --depth 1 /flutter
     git config --global --add safe.directory /flutter
-    sudo chmod -R 777 /flutter
+    chmod -R 777 /flutter
     flutter --version
 fi
-sudo apt-get clean
+apt-get clean
